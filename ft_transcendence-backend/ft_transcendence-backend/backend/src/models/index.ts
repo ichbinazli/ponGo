e63b5ad1bf7a0371b1cc843084442ec011dc83df
Enes Ali Germen <130592686+EnessAli@users.noreply.github.com>
2026-02-13 00:16:12 +0300

    Delete ft_transcendence-backend/ft_transcendence-backend directory

--- ft_transcendence-backend/ft_transcendence-backend/backend/src/models/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all models
-export * from './user.model.js';
-export * from './session.model.js';
-export * from './friendship.model.js';
-export * from './match.model.js';
-export * from './tournament.model.js';
